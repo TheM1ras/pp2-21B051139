021427494a42e7fddfe5d45760c8cd2df5b45736
TheM1ras <123150589+TheM1ras@users.noreply.github.com>
2023-02-10 11:03:19 +0600

    Update 7.py

--- TSIS3/Functions 1/7.py
+++ TSIS3/Functions 1/7.py
@@ -5,6 +5,3 @@ def has_33(nums):
             OK = True
     print(True) if OK == True else print(False)
 
-has_33([1, 3, 3])      #→ True
-has_33([1, 3, 1, 3])   #→ False
-has_33([3, 1, 3])      #→ False
