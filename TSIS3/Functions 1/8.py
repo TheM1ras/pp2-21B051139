de93b09d1eb3236a4319c99f8c1bc65ed625c533
TheM1ras <123150589+TheM1ras@users.noreply.github.com>
2023-02-10 11:02:57 +0600

    Update 8.py

--- TSIS3/Functions 1/8.py
+++ TSIS3/Functions 1/8.py
@@ -1,5 +1,5 @@
 def spy_game(nums):
-    '''
+    
     a = []
     checker = [0, 0, 7]
     for i in range(len(nums)):
@@ -14,14 +14,12 @@ def spy_game(nums):
             if nums[i] not in a:
                 a.append(nums[i])
     print(True) if a == checker else print(False)     
-    '''
+    
     for i in range(len(nums) - 2):
         if nums[i] == nums[i + 1] and int(nums[i]) == 0 and int(nums[i + 2]) == 7:
             print(True)
             break
     else: print(False)        
 
-spy_game([1,2,4,0,0,7,5]) #--> True
-spy_game([1,0,2,4,0,5,7]) #--> True
-spy_game([1,7,2,0,4,5,0]) #--> False
+
 spy_game(input().split())
